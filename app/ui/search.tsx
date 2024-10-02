3dce17d15859ed5ac817721f07a79f1c516a8e5e
Cassie Smith <lilpainthorse@byui.edu>
2024-10-02 11:10:14 -0700

    Chapter 11 Debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  // Define the debounced function outside of handleSearch
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -18,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300); // Debounce for 300ms
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -29,7 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value);
+          handleSearch(e.target.value); // Use the debounced function here
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
